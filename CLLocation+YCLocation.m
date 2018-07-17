c94de5057ff38f3ec3f968475d8a65af17e2eb22
ZYJJerry <912327710@qq.com>
2018-07-17 14:07:42 +0800

    修改CLLocation+YCLocation.m

--- CLLocation+YCLocation.m
+++ CLLocation+YCLocation.m
@@ -90,7 +90,7 @@ bool transform_sino_out_china(double lat, double lon)
 
 double transform_earth_from_mars_lat(double x, double y)
 {
-    double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * sqrt(abs(x));
+    double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * sqrt(fabs(x));
     ret += (20.0 * sin(6.0 * x * M_PI) + 20.0 * sin(2.0 * x * M_PI)) * 2.0 / 3.0;
     ret += (20.0 * sin(y * M_PI) + 40.0 * sin(y / 3.0 * M_PI)) * 2.0 / 3.0;
     ret += (160.0 * sin(y / 12.0 * M_PI) + 320 * sin(y * M_PI / 30.0)) * 2.0 / 3.0;
@@ -99,7 +99,7 @@ double transform_earth_from_mars_lat(double x, double y)
 
 double transform_earth_from_mars_lng(double x, double y)
 {
-    double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * sqrt(abs(x));
+    double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * sqrt(fabs(x));
     ret += (20.0 * sin(6.0 * x * M_PI) + 20.0 * sin(2.0 * x * M_PI)) * 2.0 / 3.0;
     ret += (20.0 * sin(x * M_PI) + 40.0 * sin(x / 3.0 * M_PI)) * 2.0 / 3.0;
     ret += (150.0 * sin(x / 12.0 * M_PI) + 300.0 * sin(x / 30.0 * M_PI)) * 2.0 / 3.0;
--- CYJHelper.podspec
+++ CYJHelper.podspec
@@ -1,9 +1,9 @@
 Pod::Spec.new do |s|
 s.name = 'CYJHelper'
-s.version = '1.0.0'
+s.version = '1.0.1'
 s.license = 'MIT'
 s.summary = 'Tools for iOS development.'
-s.homepage = 'https://github.com/ZYJZCY/CYJHelper'
+s.homepage = "https://github.com/ZYJZCY/CYJHelper"
 s.authors = { 'ZCYZYJ' => '912327710@qq.com' }
 s.source = { :git => 'https://github.com/ZYJZCY/CYJHelper.git', :tag => s.version.to_s }
 s.requires_arc = true
